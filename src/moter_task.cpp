882362ddde34502d5bffa8310c68a33f0dbb8ca6
Toshiki Goto <gotoshi.euphonium@gmail.com>
2024-01-11 00:27:06 +0900

    add servo

--- src/moter_task.cpp
+++ src/moter_task.cpp
@@ -1,48 +1,62 @@
 #include "moter_task.hpp"
-#include "util.hpp"
-
-#include "ESP32_Servo.h"
 
 static const char *TAG = "moter_task";
 
-QueueHandle_t g_moter_queue;
+QueueHandle_t moter_queue;
+
+void change_speed(const speed_t speed) {
+    ESP_LOGI(TAG, "Changing speed");
+    if (moter_queue == NULL) {
+        ESP_LOGE(TAG, "g_moter_queue is NULL");
+        myexit(1);
+    }
+    if (xQueueSendToBack(moter_queue, (void *)&speed, 10) != pdTRUE) {
+        ESP_LOGE(TAG, "Failed to send speed");
+        myexit(1);
+    }
+}
 
 void moter_task(void *arg) {
     moter_driver_t *driver = (moter_driver_t *)arg;
     speed_t speed = 0;
 
     while (true) {
-        if (xQueueReceive(g_moter_queue, &speed, 10) == pdTRUE) {
+        if (xQueueReceive(moter_queue, &speed, 10) == pdTRUE) {
             ESP_LOGI(TAG, "speed: %d", (uint8_t)speed);
             driver->exec(speed);
         }
     }
 }
 
-void moter_driver_t::send_pwm_signal(speed_t speed) {
-    // TODO
+inline void moter_driver_t::send_pwm_signal(const speed_t speed) {
+    servo.write(speed);
 }
 
-void moter_driver_t::servo_cfg_init() {
-    // TODO
+inline void moter_driver_t::servo_cfg_init() {
+    servo.setPeriodHertz(50);
+    servo.attach(out, MOTER_MIN_SIGNAL, MOTER_MAX_SIGNAL);
 }
 
 void moter_driver_t::init() {
 
     ESP_LOGI(TAG, "Initializing Moter Task Manager");
-    g_moter_queue = xQueueCreate(MOTER_QUEUE_SIZE, sizeof(speed_t));
-    if (g_moter_queue == NULL) {
+    moter_queue = xQueueCreate(MOTER_QUEUE_SIZE, sizeof(speed_t));
+    if (moter_queue == NULL) {
         ESP_LOGE(TAG, "Failed to create moter queue");
         exit(1);
     }
     servo_cfg_init();
 
     // init servo driver
-    send_pwm_signal(MOTER_MAX_SPEED);
+    send_pwm_signal(MOTER_MAX_SIGNAL);
     vTaskMilliSecondDelay(2000);
 
-    send_pwm_signal(MOTER_MIN_SPEED);
+    send_pwm_signal(MOTER_MIN_SIGNAL);
     vTaskMilliSecondDelay(1000);
+}
+
+inline signal_t moter_driver_t::speed_to_signal(const speed_t speed) {
+    return (MOTER_MAX_SIGNAL - MOTER_MIN_SIGNAL) / (MOTER_MAX_SPEED - MOTER_MIN_SPEED) * speed + MOTER_MIN_SIGNAL;
 }
 
 void moter_driver_t::exec(speed_t speed) {
@@ -55,5 +69,5 @@ void moter_driver_t::exec(speed_t speed) {
         ESP_LOGW(TAG, "speed is under MOTER_MIN_SPEED");
     }
 
-    send_pwm_signal(speed);
+    send_pwm_signal(speed_to_signal(speed));
 }
